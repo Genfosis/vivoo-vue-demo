4544bf4ff710ac86ede7282fa811ab74c805c390
jeeramethgfs <jeerameth.k@genfosis.com>
2024-01-11 12:39:05 +0700

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,10 +5,7 @@ import { ref } from 'vue';
 import { useDisplay } from 'vuetify/lib/framework.mjs';
 defineProps<{fullName: string}>()
 const display = useDisplay()
-const isMobile = display.smAndDown
-const hydration = ref(40)
-const fitnessLevel = ref(70)
-const eatingHabit = ref(30)
+
 
 </script>
 <template>
@@ -79,48 +76,48 @@ const eatingHabit = ref(30)
             <v-col cols="6" md="4">
                 <v-sheet rounded :elevation="5" class="stats-container">
                     <v-container class="gfs-score-container">
-                        <h2>18K/12K</h2>
-                        <p>Steps</p>
+                        <h2>Low</h2>
+                        <p>Magnesium</p>
                     </v-container>
                 </v-sheet>
             </v-col>
             <v-col cols="6" md="4">
                 <v-sheet rounded :elevation="5" class="stats-container">
                     <v-container class="gfs-score-container">
-                        <h2>18K/12K</h2>
-                        <p>Steps</p>
+                        <h2>Low</h2>
+                        <p>Vitamin C</p>
                     </v-container>
                 </v-sheet>
             </v-col>
             <v-col cols="6" md="4">
                 <v-sheet rounded :elevation="5" class="stats-container">
                     <v-container class="gfs-score-container">
-                        <h2>18K/12K</h2>
-                        <p>Steps</p>
+                        <h2>High</h2>
+                        <p>Sodium</p>
                     </v-container>
                 </v-sheet>
             </v-col>
             <v-col cols="6" md="4">
                 <v-sheet rounded :elevation="5" class="stats-container">
                     <v-container class="gfs-score-container">
-                        <h2>18K/12K</h2>
-                        <p>Steps</p>
+                        <h2>Great</h2>
+                        <p>Protein</p>
                     </v-container>
                 </v-sheet>
             </v-col>
             <v-col cols="6" md="4">
                 <v-sheet rounded :elevation="5" class="stats-container">
                     <v-container class="gfs-score-container">
-                        <h2>18K/12K</h2>
-                        <p>Steps</p>
+                        <h2>No risk</h2>
+                        <p>Oxidative Stress</p>
                     </v-container>
                 </v-sheet>
             </v-col>
             <v-col cols="6" md="4">
                 <v-sheet rounded :elevation="5" class="stats-container">
                     <v-container class="gfs-score-container">
-                        <h2>18K/12K</h2>
-                        <p>Steps</p>
+                        <h2>Great</h2>
+                        <p>Ketone</p>
                     </v-container>
                 </v-sheet>
             </v-col>
@@ -140,63 +137,8 @@ const eatingHabit = ref(30)
     <!-- Vdsgsreadgreagggggggggggga -->
 
 
-    <v-container>
-        <v-sheet rounded :elevation="5" class="stats-container">
-        <v-container>
-            <h1>Junee Juriya</h1>
-            <h1>15-01-2024</h1>
-        <v-row class="align-center">
-            <v-col :cols="isMobile ? 12 : 6">
-            </v-col>
-            <v-col>
-                <v-container class="detail">
-                    <h1>Health Score</h1>
-                    <v-avatar class="health-score" variant="elevated" size="60" color="blue" text="70"></v-avatar>
-                    <div class="progress-bar-wrapper">
-                        <v-progress-linear
-                            v-model="hydration"
-                            rounded
-                            color="amber"
-                            height="20"
-                        ></v-progress-linear>
-                    </div>
-                    <div class="progress-bar-wrapper">
-                        <v-progress-linear
-                            rounded
-                            v-model="fitnessLevel"
-                            color="blue"
-                            height="20"
-                        ></v-progress-linear>
-                    </div>
-                    <div class="progress-bar-wrapper">
-                        <v-progress-linear
-                            v-model="eatingHabit"
-                            rounded
-                            color="amber"
-                            height="20"
-                        ></v-progress-linear>
-                    </div>
-                </v-container>
-            </v-col>
-        </v-row>
-        </v-container>
-        <v-sheet class="achievement-banner" rounded color="info"   :elevation="9">
-        <v-container >
-           <v-row class="align-center">
-            <v-col :cols="isMobile? 2: 1">
-                <v-avatar size="40" variant="tonal" text="something"></v-avatar>
-            </v-col>
-            <v-col :cols="isMobile? 10: 11">
-                <p>4 Week Exercise Streak !</p>
-            </v-col>
-           </v-row>
-        </v-container>
-        </v-sheet>
-        </v-sheet>
-    </v-container>
-    <v-container>
-       <v-btn class="butotn">Share</v-btn>
-    </v-container>
+
+
 </template>
 <style scoped>
 .home-container {
@@ -230,6 +172,7 @@ const eatingHabit = ref(30)
     align-items: center;
 }
 .gfs-score-container {
+    padding: 5px;
     padding-top: 10px;
     padding-bottom: 10px;
     display: flex;
@@ -248,7 +191,7 @@ p {
     text-align: left;
 }
 .section-name-container {
-    padding: 0px 16px 0px 16px;
+    padding: 0px 16px 0px 20px;
 }
 
 </style>
